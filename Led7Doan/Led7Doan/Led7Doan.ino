b14ab28ec4f38d236cc27bd6c258c1380782ca19
Lê Quốc Duy  60135343 <hopro178@gmail.com>
2021-10-16 11:01:40 +0700

    re-up

--- Led7Doan/Led7Doan/Led7Doan.ino
+++ Led7Doan/Led7Doan/Led7Doan.ino
@@ -17,6 +17,69 @@ void KHONG(){
   digitalWrite(f,HIGH);
   digitalWrite(g,LOW);
 }
+void MOT(){
+  digitalWrite(a,LOW);
+  digitalWrite(b,HIGH);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,LOW);
+  digitalWrite(e,LOW);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW);
+}
+void HAI(){
+  digitalWrite(a,HIGH);
+  digitalWrite(b,HIGH);
+  digitalWrite(c,LOW);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,LOW);
+  digitalWrite(g,HIGH);
+}
+void BA(){
+  digitalWrite(a,HIGH);
+  digitalWrite(b,HIGH);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,LOW);
+  digitalWrite(f,LOW);
+  digitalWrite(g,HIGH);
+}
+void BON(){
+  digitalWrite(a,LOW);
+  digitalWrite(b,HIGH);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,LOW);
+  digitalWrite(e,LOW);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,HIGH);
+}
+void NAM(){
+  digitalWrite(a,HIGH);
+  digitalWrite(b,LOW);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,LOW);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,HIGH);
+}
+void SAU(){
+  digitalWrite(a,HIGH);
+  digitalWrite(b,LOW);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,HIGH);
+}
+void BAY(){
+  digitalWrite(a,HIGH);
+  digitalWrite(b,HIGH);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,LOW);
+  digitalWrite(e,LOW);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW);
+}
 void TAM(){
   digitalWrite(a,HIGH);
   digitalWrite(b,HIGH);
@@ -26,10 +89,34 @@ void TAM(){
   digitalWrite(f,HIGH);
   digitalWrite(g,HIGH);
 }
-
+void CHIN(){
+  digitalWrite(a,HIGH);
+  digitalWrite(b,HIGH);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,LOW);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,HIGH);
+}
 void loop() {
   KHONG();
   delay(1000);
+  MOT();
+  delay(1000);
+  HAI();
+  delay(1000);
+   BA();
+  delay(1000);
+   BON();
+  delay(1000);
+   NAM();
+  delay(1000);
+   SAU();
+  delay(1000);
+   BAY();
+  delay(1000);
   TAM();
   delay(1000);
+  CHIN();
+   delay(1000);
 }
