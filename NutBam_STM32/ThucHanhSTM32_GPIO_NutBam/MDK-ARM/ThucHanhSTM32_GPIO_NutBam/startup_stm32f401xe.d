5ec071979d181827cf37e631fbee7081e3a1dcbb
Lê Quốc Duy  60135343 <hopro178@gmail.com>
2021-11-07 12:27:11 +0700

    repair name

---
+++ NutBam_STM32/ThucHanhSTM32_GPIO_NutBam/MDK-ARM/ThucHanhSTM32_GPIO_NutBam/startup_stm32f401xe.d
@@ -0,0 +1,1 @@
+thuchanhstm32_gpio_nutbam\startup_stm32f401xe.o: startup_stm32f401xe.s
---
+++ ThucHanh1_ADC_STM32/ThucHanhSTM32_ADC/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ThucHanhSTM32_ADC' 
+ * Target:  'ThucHanhSTM32_ADC' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
